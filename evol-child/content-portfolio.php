ab7e6be3f5a4809b51018621d04ced0f990ef595
Alicia Hurst <alicia@aliciahurst.com>
2015-08-09 14:58:48 -0400

    About page changes

--- evol-child/content-portfolio.php
+++ evol-child/content-portfolio.php
@@ -10,6 +10,7 @@
 		<a href="<?php the_permalink(); ?>">
 			<?php the_post_thumbnail( 'project' ); ?>
 			<h4><?php the_title(); ?></h4>
+			<p><?php the_field('subtitle'); ?></p>
 		</a>
 
 	</div>
