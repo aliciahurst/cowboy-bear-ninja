1f526c104a6a2d376ab5b2425ffc402cd6611aec
Alicia Hurst <alicia@aliciahurst.com>
2015-08-16 12:38:46 -0400

    Add intro custom field

--- evol-child/content-portfolio-single.php
+++ evol-child/content-portfolio-single.php
@@ -1,46 +1,46 @@
 <div class="post-content">
-<div class="intro-divs">
-<div class="intro">
-<p>Getting desktop and mobile toggles styled and workingGetting desktop and mobile toggles styled and working Getting desktop and mobile toggles styled and working Getting desktop and mobile toggles styled and working Getting desktop and mobile toggles styled and working Getting desktop and mobile toggles styled and working Getting desktop and mobile toggles styled and working
-</p>
-</div>
+	<div class="intro-divs">
 
-<!-- WHAT WE DID -->
-	<?php if( have_rows('what_we_did') ): ?> 
-		<div class="one-third">
-		<h4>What We Did</h4>
-		<ul class="whatwedid">
-			<?php while( have_rows('what_we_did') ): the_row(); 
-			$service = get_sub_field('wwd_service');
-			?>
-			<li>
-				<?php echo $service; ?>
-			</li>
-		<?php endwhile; ?>
-	</ul>
-	</div>
-<?php endif; ?>
+		<div class="intro">
+			<?php the_field('intro_text'); ?>
+		</div>
 
-<!-- CREDITS -->
+		<!-- WHAT WE DID -->
+		<?php if( have_rows('what_we_did') ): ?> 
+			<div class="one-third">
+				<h4>What We Did</h4>
+				<ul class="whatwedid">
+					<?php while( have_rows('what_we_did') ): the_row(); 
+					$service = get_sub_field('wwd_service');
+					?>
+					<li>
+						<?php echo $service; ?>
+					</li>
+				<?php endwhile; ?>
+			</ul>
+		</div>
+	<?php endif; ?>
+
+	<!-- CREDITS -->
 	<?php if( have_rows('credits') ): ?> 
 		<div class="one-third">
-		<h4>Credits</h4>
-		<ul class="credits">
-			<?php while( have_rows('credits') ): the_row(); 
-			$role = get_sub_field('credits_role');
-			$name = get_sub_field('credits_name');
-			$link = get_sub_field('credits_link');
-			?>
-			<li>
-				<span class="credits-role"><?php echo $role; ?></span>
-				<?php if( $link ): ?>
-					<a href="<?php echo $link; ?>"><?php echo $name; ?></a>
-				<?php else: ?>
-					<?php echo $name; ?>
-				<?php endif; ?>
-			</li>
-		<?php endwhile; ?>
-	</ul>
+			<h4>Credits</h4>
+			<ul class="credits">
+				<?php while( have_rows('credits') ): the_row(); 
+				$role = get_sub_field('credits_role');
+				$name = get_sub_field('credits_name');
+				$link = get_sub_field('credits_link');
+				?>
+				<li>
+					<span class="credits-role"><?php echo $role; ?></span>
+					<?php if( $link ): ?>
+						<a href="<?php echo $link; ?>"><?php echo $name; ?></a>
+					<?php else: ?>
+						<?php echo $name; ?>
+					<?php endif; ?>
+				</li>
+			<?php endwhile; ?>
+		</ul>
 	</div>
 <?php endif; ?>
 </div>
